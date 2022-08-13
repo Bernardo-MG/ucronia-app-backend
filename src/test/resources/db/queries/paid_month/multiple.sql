3a4a25ef8b777a4e3f932130da8e663aeb875613
Bernardo <programming@bernardomg.com>
2022-08-13 13:32:33 +0200

    Added tests

---
+++ src/test/resources/db/queries/paid_month/multiple.sql
@@ -0,0 +1,6 @@
+INSERT INTO paid_months (member, paid_month, paid_year) VALUES
+   (1, 2, 3),
+   (2, 2, 3),
+   (3, 2, 3),
+   (4, 2, 3),
+   (5, 2, 3);
---
+++ src/test/resources/db/queries/paid_month/single.sql
@@ -0,0 +1,2 @@
+INSERT INTO paid_months (member, paid_month, paid_year) VALUES
+   (1, 2, 3);
