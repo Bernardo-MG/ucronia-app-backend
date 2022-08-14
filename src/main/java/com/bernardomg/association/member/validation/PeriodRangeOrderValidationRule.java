92a0df85685ee76097b6222d62866302a04cc4da
Bernardo <programming@bernardomg.com>
2022-08-14 03:06:38 +0200

    Using factory method for errors

--- src/main/java/com/bernardomg/association/member/validation/PeriodRangeOrderValidationRule.java
+++ src/main/java/com/bernardomg/association/member/validation/PeriodRangeOrderValidationRule.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import com.bernardomg.association.member.model.MemberPeriod;
-import com.bernardomg.validation.DefaultValidationError;
 import com.bernardomg.validation.ValidationError;
 import com.bernardomg.validation.ValidationRule;
 
@@ -18,20 +17,18 @@ public final class PeriodRangeOrderValidationRule implements ValidationRule<Memb
     @Override
     public Collection<ValidationError> test(final MemberPeriod period) {
         final Collection<ValidationError> result;
-        DefaultValidationError            error;
+        final ValidationError             error;
 
         result = new ArrayList<>();
         if (period.getStartYear() > period.getEndYear()) {
             // Starting year after end year
-            error = new DefaultValidationError();
-            error.setError("error.memberPeriod.startYearAfterEndYear");
+            error = ValidationError.of("error.memberPeriod.startYearAfterEndYear");
             result.add(error);
         } else if ((period.getStartYear()
             .equals(period.getEndYear())) && (period.getStartMonth() > period.getEndMonth())) {
             // Same year
             // Starting month after end month
-            error = new DefaultValidationError();
-            error.setError("error.memberPeriod.startMonthAfterEndMonth");
+            error = ValidationError.of("error.memberPeriod.startMonthAfterEndMonth");
             result.add(error);
         }
 
--- src/main/java/com/bernardomg/validation/ValidationError.java
+++ src/main/java/com/bernardomg/validation/ValidationError.java
@@ -3,6 +3,15 @@ package com.bernardomg.validation;
 
 public interface ValidationError {
 
+    public static ValidationError of(final String code) {
+        final DefaultValidationError error;
+
+        error = new DefaultValidationError();
+        error.setError("error.memberPeriod.startMonthAfterEndMonth");
+
+        return error;
+    }
+
     public String getError();
 
 }
