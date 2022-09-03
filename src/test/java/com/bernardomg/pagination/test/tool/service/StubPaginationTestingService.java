c76a8e6c402b8fd261f66f7d82dc5c4e83045448
Bernardo <programming@bernardomg.com>
2022-09-03 16:43:24 +0200

    Added default implementation of test service

---
+++ src/test/java/com/bernardomg/pagination/test/tool/service/StubPaginationTestingService.java
@@ -0,0 +1,19 @@
+
+package com.bernardomg.pagination.test.tool.service;
+
+import java.util.Collections;
+
+import org.springframework.stereotype.Service;
+
+import com.bernardomg.mvc.pagination.model.Pagination;
+import com.bernardomg.mvc.pagination.model.Sort;
+
+@Service
+public final class StubPaginationTestingService implements PaginationTestingService {
+
+    @Override
+    public final Iterable<String> getAll(final Pagination pagination, final Sort sort) {
+        return Collections.emptyList();
+    }
+
+}
