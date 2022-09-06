cfe49619561210f58fcd768d6c23b25c6538a69a
Bernardo <programming@bernardomg.com>
2022-09-06 12:27:36 +0200

    Added tests for the exception handler

---
+++ src/test/java/com/bernardomg/mvc/error/test/unit/ITControllerErrorResponse.java
@@ -0,0 +1,102 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2022 the original author or authors.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.bernardomg.mvc.error.test.unit;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.bernardomg.association.test.config.annotation.MvcIntegrationTest;
+import com.bernardomg.mvc.error.test.util.controller.ErrorTestController;
+
+@MvcIntegrationTest
+@DisplayName("Controller advices - error response structure")
+public final class ITControllerErrorResponse {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    public ITControllerErrorResponse() {
+        super();
+    }
+
+    @Test
+    @DisplayName("Returns the response structure for method argument errors")
+    public final void testErrorHandling_MethodArgumentError_Response() throws Exception {
+        final ResultActions result;
+
+        result = mockMvc.perform(getMethodArgumentRequest());
+
+        // The operation was rejected
+        result.andExpect(MockMvcResultMatchers.status()
+            .isBadRequest());
+
+        // The response model contains the expected attributes
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.content")
+            .exists());
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.content", Matchers.hasSize(1)));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.content[0].field", Matchers.equalTo("name")));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.content[0].value", Matchers.equalTo(null)));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.content[0].object", Matchers.equalTo("errorTestObject")));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.content[0].message", Matchers.equalTo("must not be null")));
+    }
+
+    @Test
+    @Disabled
+    @DisplayName("Returns the response structure for validation errors")
+    public final void testErrorHandling_ValidationError_Response() throws Exception {
+        final ResultActions result;
+
+        result = mockMvc.perform(getValidationRequest());
+
+        // The operation was rejected
+        result.andExpect(MockMvcResultMatchers.status()
+            .isInternalServerError());
+
+        // The response model contains the expected attributes
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.content")
+            .exists());
+    }
+
+    private final RequestBuilder getMethodArgumentRequest() {
+        return MockMvcRequestBuilders.post(ErrorTestController.PATH_METHOD_ARG)
+            .content("{}")
+            .contentType(MediaType.APPLICATION_JSON);
+    }
+
+    private final RequestBuilder getValidationRequest() {
+        return MockMvcRequestBuilders.get(ErrorTestController.PATH_VALIDATION)
+            .contentType(MediaType.APPLICATION_JSON);
+    }
+
+}
---
+++ src/test/java/com/bernardomg/mvc/error/test/util/controller/ErrorTestController.java
@@ -0,0 +1,45 @@
+
+package com.bernardomg.mvc.error.test.util.controller;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bernardomg.mvc.error.test.util.model.ErrorTestObject;
+import com.bernardomg.validation.exception.ValidationException;
+
+@RestController
+@RequestMapping(ErrorTestController.PATH)
+public class ErrorTestController {
+
+    public static final String PATH            = "/test/error";
+
+    public static final String PATH_METHOD_ARG = PATH + "/methodArg";
+
+    public static final String PATH_VALIDATION = PATH + "/validation";
+
+    public ErrorTestController() {
+        super();
+    }
+
+    @PostMapping(path = "/methodArg", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ErrorTestObject exceptionMethodArg(@Valid @RequestBody final ErrorTestObject arg)
+            throws MethodArgumentNotValidException {
+        return arg;
+    }
+
+    @GetMapping(path = "/validation", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Object> exceptionValidation() {
+        throw new ValidationException(Collections.emptyList());
+    }
+
+}
