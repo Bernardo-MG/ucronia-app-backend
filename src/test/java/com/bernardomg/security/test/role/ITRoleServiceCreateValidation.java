1febd003e2de187369c364ef800ce5ac794b83cd
Bernardo <programming@bernardomg.com>
2022-10-23 16:24:08 +0200

    Added tests

--- src/test/java/com/bernardomg/security/test/role/ITRoleServiceCreateValidation.java
+++ src/test/java/com/bernardomg/security/test/role/ITRoleServiceCreateValidation.java
@@ -1,6 +1,9 @@
 
 package com.bernardomg.security.test.role;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,7 +11,9 @@ import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.bernardomg.association.test.config.annotation.IntegrationTest;
+import com.bernardomg.security.model.DtoPrivilege;
 import com.bernardomg.security.model.DtoRole;
+import com.bernardomg.security.model.Privilege;
 import com.bernardomg.security.service.RoleService;
 import com.bernardomg.validation.exception.ValidationException;
 
@@ -55,6 +60,33 @@ public class ITRoleServiceCreateValidation {
         exception = Assertions.assertThrows(ValidationException.class, executable);
 
         Assertions.assertEquals("error.name.missing", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Throws an exception when the privilege doesn't exists")
+    public void testCreate_NotExistingPrivilege() {
+        final DtoRole               data;
+        final DtoPrivilege          privilege;
+        final Executable            executable;
+        final Exception             exception;
+        final Collection<Privilege> privileges;
+
+        privilege = new DtoPrivilege();
+        privilege.setId(111L);
+
+        data = new DtoRole();
+        data.setName("ADMIN");
+
+        privileges = new ArrayList<>();
+        privileges.add(privilege);
+
+        data.setPrivileges(privileges);
+
+        executable = () -> service.create(data);
+
+        exception = Assertions.assertThrows(ValidationException.class, executable);
+
+        Assertions.assertEquals("error.privilege.notExisting", exception.getMessage());
     }
 
 }
