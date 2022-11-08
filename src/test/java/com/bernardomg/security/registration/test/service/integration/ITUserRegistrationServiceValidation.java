e1117916adaccdb436755057c851e10f55b80f51
Bernardo-MG <programming@bernardo-mg.com>
2022-11-08 23:18:45 +0100

    Corrected test

--- src/test/java/com/bernardomg/security/registration/test/service/integration/ITUserRegistrationServiceValidation.java
+++ src/test/java/com/bernardomg/security/registration/test/service/integration/ITUserRegistrationServiceValidation.java
@@ -30,7 +30,7 @@ public class ITUserRegistrationServiceValidation {
         final Executable executable;
         final Exception  exception;
 
-        executable = () -> service.registerUser("admin", "", "1234");
+        executable = () -> service.registerUser("abc", "", "1234");
 
         exception = Assertions.assertThrows(ValidationException.class, executable);
 
@@ -56,7 +56,7 @@ public class ITUserRegistrationServiceValidation {
         final Executable executable;
         final Exception  exception;
 
-        executable = () -> service.registerUser("admin", "abc", "1234");
+        executable = () -> service.registerUser("abc", "abc", "1234");
 
         exception = Assertions.assertThrows(ValidationException.class, executable);
 
