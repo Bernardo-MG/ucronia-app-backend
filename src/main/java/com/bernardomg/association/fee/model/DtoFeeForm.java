14af639f0a63d07dee40d1428d8071d1dd6fd303
Bernardo-MG <programming@bernardo-mg.com>
2023-05-03 22:23:30 +0200

    Set format for DTO dates

--- src/main/java/com/bernardomg/association/fee/model/DtoFeeForm.java
+++ src/main/java/com/bernardomg/association/fee/model/DtoFeeForm.java
@@ -5,11 +5,14 @@ import java.util.Calendar;
 
 import javax.validation.constraints.NotNull;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 
 @Data
 public final class DtoFeeForm implements FeeForm {
 
+    @DateTimeFormat(pattern = "yyyy-MM")
     @NotNull
     private Calendar date;
 
--- src/main/java/com/bernardomg/association/transaction/model/DtoTransactionForm.java
+++ src/main/java/com/bernardomg/association/transaction/model/DtoTransactionForm.java
@@ -6,6 +6,8 @@ import java.util.Calendar;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 
 @Data
@@ -14,6 +16,7 @@ public final class DtoTransactionForm implements TransactionForm {
     @NotNull
     private Float    amount;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     @NotNull
     private Calendar date;
 
