0845921a571099eaec1f1483d29f366ffbaf1194
Bernardo <programming@bernardomg.com>
2023-06-22 13:45:03 +0200

    Removed validations from query DTOs

--- src/main/java/com/bernardomg/association/member/model/request/ValidatedMemberQuery.java
+++ src/main/java/com/bernardomg/association/member/model/request/ValidatedMemberQuery.java
@@ -1,8 +1,6 @@
 
 package com.bernardomg.association.member.model.request;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,22 +12,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public final class ValidatedMemberQuery implements MemberQuery {
 
-    @NotNull
     private Boolean active;
 
     private Long    id;
 
-    @NotNull
     private String  identifier;
 
-    @NotEmpty
-    @NotNull
     private String  name;
 
-    @NotNull
     private String  phone;
 
-    @NotNull
     private String  surname;
 
 }
--- src/main/java/com/bernardomg/association/transaction/model/request/ValidatedTransactionQuery.java
+++ src/main/java/com/bernardomg/association/transaction/model/request/ValidatedTransactionQuery.java
@@ -5,7 +5,6 @@ import java.util.Calendar;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,15 +17,12 @@ import lombok.NoArgsConstructor;
 public final class ValidatedTransactionQuery implements TransactionQuery {
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @NotNull
     private Calendar date;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @NotNull
     private Calendar endDate;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @NotNull
     private Calendar startDate;
 
 }
