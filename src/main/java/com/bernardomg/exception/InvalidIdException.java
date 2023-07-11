30ec796ffed30411237c199c56347e113f6eb5e1
Bernardo-MG <programming@bernardomg.com>
2023-07-12 00:54:47 +0200

    Formatted code

--- src/main/java/com/bernardomg/exception/InvalidIdException.java
+++ src/main/java/com/bernardomg/exception/InvalidIdException.java
@@ -1,25 +1,24 @@
-package com.bernardomg.exception;
 
+package com.bernardomg.exception;
 
 public final class InvalidIdException extends RuntimeException {
 
     private static final long serialVersionUID = -1589290420417721821L;
-    
-    private final Object id;
+
+    private final Object      id;
 
     public InvalidIdException(final Object id) {
         super();
-        
+
         this.id = id;
     }
 
     public InvalidIdException(final String message, final Object identifier) {
         super(message);
-        
+
         id = identifier;
     }
 
-    
     public Object getId() {
         return id;
     }
