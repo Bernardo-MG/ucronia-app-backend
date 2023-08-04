4fb34702db9aaa9cb6966b4a38e98e34225fa64f
Bernardo <programming@bernardomg.com>
2023-08-04 13:48:46 +0200

    Formatted code

--- src/main/java/com/bernardomg/security/user/exception/UserNotFoundException.java
+++ src/main/java/com/bernardomg/security/user/exception/UserNotFoundException.java
@@ -6,8 +6,7 @@ public final class UserNotFoundException extends RuntimeException {
     private static final long serialVersionUID = -7226031317959127168L;
 
     public UserNotFoundException(final String username) {
-        super(
-            String.format("Couldn't find user %s", username));
+        super(String.format("Couldn't find user %s", username));
     }
 
 }
