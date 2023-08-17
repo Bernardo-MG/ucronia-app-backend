35afa127b4ffc140e47870bdcd1a5aac56e3eb7b
Bernardo-MG <programming@bernardomg.com>
2023-08-18 00:34:26 +0200

    Added async executor

--- src/main/java/com/bernardomg/association/config/AsyncConfig.java
+++ src/main/java/com/bernardomg/association/config/AsyncConfig.java
@@ -1,10 +1,27 @@
+
 package com.bernardomg.association.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @EnableAsync
 public class AsyncConfig {
+
+    @Bean
+    public ThreadPoolTaskExecutor asyncExecutor() {
+        ThreadPoolTaskExecutor executor;
+
+        executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10); // Set the number of initial threads
+        executor.setMaxPoolSize(20); // Set the maximum number of threads
+        executor.setQueueCapacity(100); // Set the capacity of the queue
+        executor.setThreadNamePrefix("Async-"); // Set thread name prefix
+        executor.initialize();
+
+        return executor;
+    }
 
 }
