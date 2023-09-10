7b13c9dddedab25537295997b5a0a31482a3a2f8
Bernardo-MG <programming@bernardomg.com>
2023-09-11 00:01:53 +0200

    Sorts data for test

--- src/test/java/com/bernardomg/association/test/fee/util/assertion/FeeAssertions.java
+++ src/test/java/com/bernardomg/association/test/fee/util/assertion/FeeAssertions.java
@@ -19,7 +19,7 @@ public final class FeeAssertions {
             .withFailMessage("Expected name '%s' but got '%s'", expected.getMemberName(), received.getMemberName())
             .isEqualTo(expected.getMemberName());
         Assertions.assertThat(received.getDate())
-            .withFailMessage("Expected date '%s' but got '%s'", received.getDate(), expected.getDate())
+            .withFailMessage("Expected date '%s' but got '%s'", expected.getDate(), received.getDate())
             .isEqualTo(expected.getDate());
         Assertions.assertThat(received.getPaid())
             .withFailMessage("Expected paid flag '%s' but got '%s'", expected.getPaid(), received.getPaid())
@@ -34,7 +34,7 @@ public final class FeeAssertions {
             .withFailMessage("Expected member id '%s' but got '%s'", expected.getMemberId(), received.getMemberId())
             .isEqualTo(expected.getMemberId());
         Assertions.assertThat(received.getDate())
-            .withFailMessage("Expected date '%s' but got '%s'", received.getDate(), expected.getDate())
+            .withFailMessage("Expected date '%s' but got '%s'", expected.getDate(), received.getDate())
             .isEqualTo(expected.getDate());
         Assertions.assertThat(received.getPaid())
             .withFailMessage("Expected paid flag '%s' but got '%s'", expected.getPaid(), received.getPaid())
