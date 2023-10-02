b4b01ee3f7124a151b664a675181f8172a7d010c
Bernardo-MG <programming@bernardomg.com>
2023-10-03 00:29:11 +0200

    Removed action and resource controllers

--- src/main/java/com/bernardomg/security/permission/config/PermissionConfig.java
+++ src/main/java/com/bernardomg/security/permission/config/PermissionConfig.java
@@ -27,22 +27,14 @@ package com.bernardomg.security.permission.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.bernardomg.security.permission.model.mapper.ActionMapper;
 import com.bernardomg.security.permission.model.mapper.PermissionMapper;
-import com.bernardomg.security.permission.model.mapper.ResourceMapper;
 import com.bernardomg.security.permission.model.mapper.RolePermissionMapper;
-import com.bernardomg.security.permission.persistence.repository.ActionRepository;
 import com.bernardomg.security.permission.persistence.repository.PermissionRepository;
-import com.bernardomg.security.permission.persistence.repository.ResourceRepository;
 import com.bernardomg.security.permission.persistence.repository.RoleGrantedPermissionRepository;
 import com.bernardomg.security.permission.persistence.repository.RolePermissionRepository;
-import com.bernardomg.security.permission.service.ActionService;
-import com.bernardomg.security.permission.service.DefaultActionService;
 import com.bernardomg.security.permission.service.DefaultPermissionService;
-import com.bernardomg.security.permission.service.DefaultResourceService;
 import com.bernardomg.security.permission.service.DefaultRolePermissionService;
 import com.bernardomg.security.permission.service.PermissionService;
-import com.bernardomg.security.permission.service.ResourceService;
 import com.bernardomg.security.permission.service.RolePermissionService;
 import com.bernardomg.security.user.persistence.repository.RoleRepository;
 
@@ -59,20 +51,10 @@ public class PermissionConfig {
         super();
     }
 
-    @Bean("actionService")
-    public ActionService getActionService(final ActionRepository repository, final ActionMapper mapper) {
-        return new DefaultActionService(repository, mapper);
-    }
-
     @Bean("permissionService")
     public PermissionService getPermissionService(final PermissionRepository repository,
             final PermissionMapper mapper) {
         return new DefaultPermissionService(repository, mapper);
-    }
-
-    @Bean("resourceService")
-    public ResourceService getResourceService(final ResourceRepository repository, final ResourceMapper mapper) {
-        return new DefaultResourceService(repository, mapper);
     }
 
     @Bean("rolePermissionService")
