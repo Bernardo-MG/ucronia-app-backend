6c1acd20aebcc9c6676424dd1a965612ef2cfaf9
Bernardo-MG <programming@bernardomg.com>
2023-10-11 17:14:54 +0200

    Proxying target class

--- src/main/java/com/bernardomg/config/CacheConfig.java
+++ src/main/java/com/bernardomg/config/CacheConfig.java
@@ -5,7 +5,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableCaching
+@EnableCaching(proxyTargetClass = true)
 public class CacheConfig {
 
     public CacheConfig() {
