b9c74a31447d3c93f5908ab5167d66e3e1664c08
Bernardo-MG <programming@bernardomg.com>
2023-10-12 22:22:46 +0200

    Extended configuration

--- src/main/java/com/bernardomg/config/CacheConfig.java
+++ src/main/java/com/bernardomg/config/CacheConfig.java
@@ -1,8 +1,13 @@
 
 package com.bernardomg.config;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.github.benmanes.caffeine.cache.CaffeineSpec;
 
 @Configuration
 @EnableCaching(proxyTargetClass = true)
@@ -10,6 +15,25 @@ public class CacheConfig {
 
     public CacheConfig() {
         super();
+    }
+
+    @Bean("CacheManager")
+    public CacheManager getCacheManager(final CacheProperties cacheProperties) {
+        final CaffeineCacheManager cacheManager;
+
+        cacheManager = new CaffeineCacheManager();
+        cacheManager.setCacheNames(cacheProperties.getCacheNames());
+
+        return cacheManager;
+    }
+
+    @Bean("CaffeineSpec")
+    public CaffeineSpec getCaffeineSpec(final CacheProperties cacheProperties) {
+        final String spec;
+
+        spec = String.format("maximumSize=%d,expireAfterAccess=%ds", cacheProperties.getMaximumSize(),
+            cacheProperties.getExpireAfterAccess());
+        return CaffeineSpec.parse(spec);
     }
 
 }
