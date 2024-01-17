5b6535c4ed1428e335878dba5843b997c051f7cc
Bernardo-MG <programming@bernardomg.com>
2024-01-17 23:57:01 +0100

    Added Archunit

---
+++ src/test/java/com/bernardomg/association/architecture/test/LayeredArchitectureTest.java
@@ -0,0 +1,28 @@
+package com.bernardomg.association.architecture.test;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+
+import com.tngtech.archunit.core.importer.ImportOption;
+
+@AnalyzeClasses(packages = "com.bernardomg.association", importOptions = ImportOption.DoNotIncludeTests.class)
+public class LayeredArchitectureTest {
+    
+    @ArchTest
+    static final ArchRule layer_dependencies_are_respected = layeredArchitecture().consideringAllDependencies()
+
+            .layer("Controllers").definedBy("com.bernardomg.association..controller..")
+            .layer("Services").definedBy("com.bernardomg.association..service..")
+            .layer("Persistence").definedBy("com.bernardomg.association..persistence..")
+            .layer("Configuration").definedBy("com.bernardomg.association..config..")
+            .layer("Validation").definedBy("com.bernardomg.association..validation..")
+            .layer("Schedule").definedBy("com.bernardomg.association..schedule..")
+
+            .whereLayer("Controllers").mayNotBeAccessedByAnyLayer()
+            .whereLayer("Services").mayOnlyBeAccessedByLayers("Configuration", "Services", "Schedule", "Controllers")
+            .whereLayer("Persistence").mayOnlyBeAccessedByLayers("Configuration", "Validation", "Persistence", "Services");
+
+}
