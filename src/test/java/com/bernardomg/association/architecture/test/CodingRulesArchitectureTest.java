3b24f08bdd6fa3628448be098445688cacd2f296
Bernardo <programming@bernardomg.com>
2024-01-18 11:11:27 +0100

    Added test rules

--- src/test/java/com/bernardomg/association/architecture/test/CodingRulesArchitectureTest.java
+++ src/test/java/com/bernardomg/association/architecture/test/CodingRulesArchitectureTest.java
@@ -1,8 +1,11 @@
 
 package com.bernardomg.association.architecture.test;
 
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import static com.tngtech.archunit.library.DependencyRules.NO_CLASSES_SHOULD_DEPEND_UPPER_PACKAGES;
 import static com.tngtech.archunit.library.GeneralCodingRules.NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS;
+
+import org.springframework.web.bind.annotation.RestController;
 
 import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
@@ -13,9 +16,23 @@ import com.tngtech.archunit.lang.ArchRule;
 public class CodingRulesArchitectureTest {
 
     @ArchTest
-    static final ArchRule  no_accesses_to_upper_package = NO_CLASSES_SHOULD_DEPEND_UPPER_PACKAGES;
+    static final ArchRule        controllers_should_be_suffixed = classes().that()
+        .areAnnotatedWith(RestController.class)
+        .should()
+        .haveSimpleNameEndingWith("Controller");
 
     @ArchTest
-    private final ArchRule no_generic_exceptions        = NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS;
+    static final ArchRule  no_accesses_to_upper_package   = NO_CLASSES_SHOULD_DEPEND_UPPER_PACKAGES;
+
+    @ArchTest
+    static final ArchRule        services_should_be_suffixed    = classes().that()
+        .resideInAPackage("..service..")
+        .and()
+        .haveNameNotMatching("*.package-info")
+        .should()
+        .haveSimpleNameEndingWith("Service");
+
+    @ArchTest
+    private final ArchRule no_generic_exceptions          = NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS;
 
 }
