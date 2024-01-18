902fd497984b6e01dd1278b2d12f7dd2005dd9fa
Bernardo <programming@bernardomg.com>
2024-01-18 12:50:36 +0100

    Sorted data config

--- src/test/java/com/bernardomg/association/auth/user/test/data/annotation/ValidUser.java
+++ src/test/java/com/bernardomg/association/auth/user/test/data/annotation/ValidUser.java
@@ -1,5 +1,5 @@
 
-package com.bernardomg.association.test.data.auth.user.annotation;
+package com.bernardomg.association.auth.user.test.data.annotation;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
@@ -14,12 +14,10 @@ import org.springframework.test.context.jdbc.Sql;
         "/db/queries/security/permission/crud.sql", "/db/queries/security/role/single.sql",
         "/db/queries/security/user/single.sql", "/db/queries/security/relationship/role_permission_granted.sql",
         "/db/queries/security/relationship/user_role.sql" })
-@Sql("/db/queries/member/single.sql")
-@Sql("/db/queries/security/relationship/user_member.sql")
 @Target({ ElementType.TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Inherited
-public @interface ValidUserWithMember {
+public @interface ValidUser {
 
 }
