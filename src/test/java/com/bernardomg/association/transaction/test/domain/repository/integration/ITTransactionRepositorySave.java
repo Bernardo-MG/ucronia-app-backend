7ea72dc92e919acb0c142d3ae6874826eb9e459a
Bernardo-MG <programming@bernardomg.com>
2024-02-06 22:42:15 +0100

    Cleaned up tests

--- src/test/java/com/bernardomg/association/transaction/test/domain/repository/integration/ITTransactionRepositorySave.java
+++ src/test/java/com/bernardomg/association/transaction/test/domain/repository/integration/ITTransactionRepositorySave.java
@@ -8,10 +8,10 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.bernardomg.association.member.test.config.data.annotation.ValidMember;
 import com.bernardomg.association.test.config.argument.DecimalArgumentsProvider;
 import com.bernardomg.association.transaction.adapter.inbound.jpa.model.TransactionEntity;
 import com.bernardomg.association.transaction.adapter.inbound.jpa.repository.TransactionSpringRepository;
+import com.bernardomg.association.transaction.config.data.annotation.PositiveTransaction;
 import com.bernardomg.association.transaction.domain.model.Transaction;
 import com.bernardomg.association.transaction.domain.repository.TransactionRepository;
 import com.bernardomg.association.transaction.test.config.factory.TransactionEntities;
@@ -27,25 +27,6 @@ class ITTransactionRepositorySave {
 
     @Autowired
     private TransactionSpringRepository springRepository;
-
-    @ParameterizedTest(name = "Amount: {0}")
-    @ArgumentsSource(DecimalArgumentsProvider.class)
-    @DisplayName("With a decimal value, the persisted transaction is returned")
-    void testCreate_Decimal_ReturnedData(final Float amount) {
-        final Transaction transactionRequest;
-        final Transaction transaction;
-
-        // GIVEN
-        transactionRequest = Transactions.amount(amount);
-
-        // WHEN
-        transaction = repository.save(transactionRequest);
-
-        // THEN
-        Assertions.assertThat(transaction.getAmount())
-            .as("amount")
-            .isEqualTo(amount);
-    }
 
     @ParameterizedTest(name = "Amount: {0}")
     @ArgumentsSource(DecimalArgumentsProvider.class)
@@ -70,9 +51,68 @@ class ITTransactionRepositorySave {
             .isEqualTo(amount);
     }
 
+    @ParameterizedTest(name = "Amount: {0}")
+    @ArgumentsSource(DecimalArgumentsProvider.class)
+    @DisplayName("With a decimal value, the persisted transaction is returned")
+    void testSave_Decimal_ReturnedData(final Float amount) {
+        final Transaction transactionRequest;
+        final Transaction transaction;
+
+        // GIVEN
+        transactionRequest = Transactions.amount(amount);
+
+        // WHEN
+        transaction = repository.save(transactionRequest);
+
+        // THEN
+        Assertions.assertThat(transaction.getAmount())
+            .as("amount")
+            .isEqualTo(amount);
+    }
+
+    @Test
+    @DisplayName("When the transaction exists it is updated")
+    @PositiveTransaction
+    void testSave_Existing_PersistedData() {
+        final Iterable<TransactionEntity> transactions;
+        final Transaction                 transaction;
+
+        // GIVEN
+        transaction = Transactions.decimal();
+
+        // WHEN
+        repository.save(transaction);
+
+        // THEN
+        transactions = springRepository.findAll();
+
+        Assertions.assertThat(transactions)
+            .as("transactions")
+            .usingRecursiveFieldByFieldElementComparatorIgnoringFields("id")
+            .containsExactly(TransactionEntities.decimal());
+    }
+
+    @Test
+    @DisplayName("When the transaction exists it is returned")
+    @PositiveTransaction
+    void testSave_Existing_ReturnedData() {
+        final Transaction created;
+        final Transaction transaction;
+
+        // GIVEN
+        transaction = Transactions.valid();
+
+        // WHEN
+        created = repository.save(transaction);
+
+        // THEN
+        Assertions.assertThat(created)
+            .as("created")
+            .isEqualTo(Transactions.valid());
+    }
+
     @Test
     @DisplayName("Persists the data")
-    @ValidMember
     void testSave_PersistedData() {
         final Iterable<TransactionEntity> transactions;
         final Transaction                 transaction;
@@ -94,7 +134,6 @@ class ITTransactionRepositorySave {
 
     @Test
     @DisplayName("Returns the created data")
-    @ValidMember
     void testSave_ReturnedData() {
         final Transaction created;
         final Transaction transaction;
