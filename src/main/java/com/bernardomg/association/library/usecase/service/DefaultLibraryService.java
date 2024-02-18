9c660da96926647af887dcc13bd708af3b2314b8
Bernardo-MG <programming@bernardomg.com>
2024-02-19 00:53:04 +0100

    Added relationships

--- src/main/java/com/bernardomg/association/library/usecase/service/DefaultLibraryService.java
+++ src/main/java/com/bernardomg/association/library/usecase/service/DefaultLibraryService.java
@@ -43,6 +43,8 @@ public final class DefaultLibraryService implements LibraryService {
 
     @Override
     public final Author createAuthor(final Author author) {
+        log.debug("Creating author {}", author);
+        
         return authorRepository.save(author);
     }
 
@@ -50,6 +52,8 @@ public final class DefaultLibraryService implements LibraryService {
     public final Book createBook(final Book book) {
         final boolean gameSystemExists;
         final boolean bookTypeExists;
+
+        log.debug("Creating book {}", book);
 
         // TODO: verify the language is a valid code
 
@@ -82,11 +86,15 @@ public final class DefaultLibraryService implements LibraryService {
 
     @Override
     public final BookType createBookType(final BookType type) {
+        log.debug("Creating book type {}", type);
+        
         return bookTypeRepository.save(type);
     }
 
     @Override
     public final GameSystem createGameSystem(final GameSystem system) {
+        log.debug("Creating game system {}", system);
+        
         return gameSystemRepository.save(system);
     }
 
