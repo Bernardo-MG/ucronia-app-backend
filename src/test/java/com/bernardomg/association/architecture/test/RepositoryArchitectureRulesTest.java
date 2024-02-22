1ea11d7a82eff1916e3b45d19dcf83f3e5fbd98b
Bernardo <programming@bernardomg.com>
2024-02-22 12:56:09 +0100

    Added tests

--- src/test/java/com/bernardomg/association/architecture/test/RepositoryArchitectureRulesTest.java
+++ src/test/java/com/bernardomg/association/architecture/test/RepositoryArchitectureRulesTest.java
@@ -1,6 +1,7 @@
 
 package com.bernardomg.association.architecture.test;
 
+import static com.bernardomg.association.architecture.config.JpaEntityClassPredicate.areJpaEntitiesClasses;
 import static com.bernardomg.association.architecture.config.SpringRepositoryClassPredicate.areSpringRepositoryClasses;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
@@ -11,6 +12,16 @@ import com.tngtech.archunit.lang.ArchRule;
 
 @AnalyzeClasses(packages = "com.bernardomg.association", importOptions = ImportOption.DoNotIncludeTests.class)
 public class RepositoryArchitectureRulesTest {
+
+    @ArchTest
+    static final ArchRule jpa_entities_should_be_in_model_package             = classes().that(areJpaEntitiesClasses())
+        .should()
+        .resideInAPackage("..adapter.inbound.jpa.model..");
+
+    @ArchTest
+    static final ArchRule jpa_entities_should_be_suffixed                     = classes().that(areJpaEntitiesClasses())
+        .should()
+        .haveSimpleNameEndingWith("Entity");
 
     @ArchTest
     static final ArchRule spring_repositories_should_be_in_repository_package = classes()
