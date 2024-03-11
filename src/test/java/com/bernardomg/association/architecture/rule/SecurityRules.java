df9b18fba98e39060b91826c971a6745056b8756
Bernardo <programming@bernardomg.com>
2024-03-11 17:20:30 +0100

    Extracted rules

--- src/test/java/com/bernardomg/association/architecture/rule/SecurityRules.java
+++ src/test/java/com/bernardomg/association/architecture/rule/SecurityRules.java
@@ -1,17 +1,14 @@
 
-package com.bernardomg.association.architecture.test;
+package com.bernardomg.association.architecture.rule;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.methods;
 
 import com.bernardomg.association.architecture.predicate.Predicates;
 import com.bernardomg.security.access.RequireResourceAccess;
-import com.tngtech.archunit.core.importer.ImportOption;
-import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 
-@AnalyzeClasses(packages = "com.bernardomg.association", importOptions = ImportOption.DoNotIncludeTests.class)
-public class SecuredArchitectureRulesTest {
+public class SecurityRules {
 
     @ArchTest
     static final ArchRule controllers_methods_should_be_secured = methods().that()
--- src/test/java/com/bernardomg/association/architecture/test/AssociationArchitectureRulesTest.java
+++ src/test/java/com/bernardomg/association/architecture/test/AssociationArchitectureRulesTest.java
@@ -2,9 +2,11 @@
 package com.bernardomg.association.architecture.test;
 
 import com.bernardomg.association.architecture.rule.CacheRules;
+import com.bernardomg.association.architecture.rule.CodingRules;
 import com.bernardomg.association.architecture.rule.ControllerRules;
 import com.bernardomg.association.architecture.rule.JpaEntityRules;
 import com.bernardomg.association.architecture.rule.RepositoryRules;
+import com.bernardomg.association.architecture.rule.SecurityRules;
 import com.bernardomg.association.architecture.rule.ServiceRules;
 import com.bernardomg.association.architecture.rule.TransactionalRules;
 import com.tngtech.archunit.core.importer.ImportOption;
@@ -19,6 +21,9 @@ public class AssociationArchitectureRulesTest {
     static final ArchTests cacheRules         = ArchTests.in(CacheRules.class);
 
     @ArchTest
+    static final ArchTests codingRules        = ArchTests.in(CodingRules.class);
+
+    @ArchTest
     static final ArchTests controllerRules    = ArchTests.in(ControllerRules.class);
 
     @ArchTest
@@ -26,6 +31,9 @@ public class AssociationArchitectureRulesTest {
 
     @ArchTest
     static final ArchTests repositoryRules    = ArchTests.in(RepositoryRules.class);
+
+    @ArchTest
+    static final ArchTests securityRules      = ArchTests.in(SecurityRules.class);
 
     @ArchTest
     static final ArchTests serviceRules       = ArchTests.in(ServiceRules.class);
