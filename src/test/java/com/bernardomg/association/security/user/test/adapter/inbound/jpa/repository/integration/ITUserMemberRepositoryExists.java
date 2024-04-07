de1dbb603fb6e2f277bbf959ffbbba1fbf2440fd
Bernardo-MG <programming@bernardomg.com>
2024-04-07 19:50:16 +0200

    Added tests

---
+++ src/test/java/com/bernardomg/association/security/user/test/adapter/inbound/jpa/repository/integration/ITUserMemberRepositoryExists.java
@@ -0,0 +1,71 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2023 the original author or authors.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.bernardomg.association.security.user.test.adapter.inbound.jpa.repository.integration;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bernardomg.association.security.user.domain.repository.UserMemberRepository;
+import com.bernardomg.association.security.user.test.config.data.annotation.ValidUserWithMember;
+import com.bernardomg.association.security.user.test.config.factory.UserConstants;
+import com.bernardomg.test.config.annotation.IntegrationTest;
+
+@IntegrationTest
+@DisplayName("UserMemberRepository - exists")
+class ITUserMemberRepositoryExists {
+
+    @Autowired
+    private UserMemberRepository repository;
+
+    @Test
+    @DisplayName("When the user exists it exists")
+    @ValidUserWithMember
+    void testGetMember() {
+        final boolean exists;
+
+        // WHEN
+        exists = repository.exists(UserConstants.USERNAME);
+
+        // THEN
+        Assertions.assertThat(exists)
+            .isTrue();
+    }
+
+    @Test
+    @DisplayName("When no data exists it doesn't exist")
+    void testGetMember_NoData() {
+        final boolean exists;
+
+        // WHEN
+        exists = repository.exists(UserConstants.USERNAME);
+
+        // THEN
+        Assertions.assertThat(exists)
+            .isFalse();
+    }
+
+}
