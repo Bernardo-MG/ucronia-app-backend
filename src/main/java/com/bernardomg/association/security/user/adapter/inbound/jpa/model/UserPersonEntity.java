359b30e601fd793ffc00530990f2ce4e25953a4b
Bernardo-MG <programming@bernardomg.com>
2024-05-12 15:36:49 +0200

    Corrected table name

--- src/main/java/com/bernardomg/association/security/user/adapter/inbound/jpa/model/UserPersonEntity.java
+++ src/main/java/com/bernardomg/association/security/user/adapter/inbound/jpa/model/UserPersonEntity.java
@@ -20,7 +20,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity(name = "UserMember")
-@Table(schema = "security", name = "user_members")
+@Table(schema = "security", name = "user_persons")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
