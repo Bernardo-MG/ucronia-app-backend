f40bb82a4182f60bb723b6b29c9f6be1ad193308
Bernardo-MG <programming@bernardomg.com>
2024-05-24 22:18:04 +0200

    Added configuration property

--- src/main/resources/db/changelog/structure/sql/initial_configuration.sql
+++ src/main/resources/db/changelog/structure/sql/initial_configuration.sql
@@ -1,3 +1,4 @@
 INSERT INTO configurations (code, config_value, value_type, restricted) VALUES
   ('fee.amount', '0', 'number', true),
-  ('social.teamup.id', '', 'string', false);
+  ('social.teamup.id', '', 'string', false),
+  ('social.googleMap.id', '', 'string', false);
