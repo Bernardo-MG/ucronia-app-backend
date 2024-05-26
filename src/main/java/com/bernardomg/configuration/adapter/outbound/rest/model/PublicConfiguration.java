9d9ed0524250da27a42822fe634e9ef62e3bbfc4
Bernardo-MG <programming@bernardomg.com>
2024-05-26 12:50:26 +0200

    Renamed fields

--- src/main/java/com/bernardomg/configuration/adapter/outbound/rest/model/PublicConfiguration.java
+++ src/main/java/com/bernardomg/configuration/adapter/outbound/rest/model/PublicConfiguration.java
@@ -1,6 +1,6 @@
 
 package com.bernardomg.configuration.adapter.outbound.rest.model;
 
-public record PublicConfiguration(String mapId, String calendarId) {
+public record PublicConfiguration(String mapCode, String calendarCode) {
 
 }
