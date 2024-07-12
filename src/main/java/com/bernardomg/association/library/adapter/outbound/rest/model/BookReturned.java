3c9e65b84fecfd81a3a2ef9acb3f93b48b9ba0c7
Bernardo-MG <programming@bernardomg.com>
2024-07-12 21:27:08 +0200

    Corrected lending DTOs

--- src/main/java/com/bernardomg/association/library/adapter/outbound/rest/model/BookReturned.java
+++ src/main/java/com/bernardomg/association/library/adapter/outbound/rest/model/BookReturned.java
@@ -5,11 +5,15 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder(setterPrefix = "with")
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookReturned {
 
     private long      book;
