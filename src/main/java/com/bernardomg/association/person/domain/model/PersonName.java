0224f078fb6fea210b64945d343c747dcd8825ac
Bernardo-MG <programming@bernardomg.com>
2024-10-02 21:42:16 +0200

    Serializes full name

--- src/main/java/com/bernardomg/association/person/domain/model/PersonName.java
+++ src/main/java/com/bernardomg/association/person/domain/model/PersonName.java
@@ -3,13 +3,16 @@ package com.bernardomg.association.person.domain.model;
 
 import org.apache.commons.lang3.StringUtils;
 
-public record PersonName(String firstName, String lastName) {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+public record PersonName(String firstName, String lastName) {
+    
     public PersonName(final String firstName, final String lastName) {
         this.firstName = StringUtils.trim(firstName);
         this.lastName = StringUtils.trim(lastName);
     }
 
+    @JsonProperty("fullName")
     public final String fullName() {
         return String.format("%s %s", firstName, lastName)
             .trim();
