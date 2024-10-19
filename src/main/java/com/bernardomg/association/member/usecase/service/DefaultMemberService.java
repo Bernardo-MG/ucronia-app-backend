fb8900348280fbbac200fa3ded1b8f40874aeed8
Bernardo-MG <programming@bernardomg.com>
2024-10-19 16:55:10 +0200

    Fixed compilation failure

--- src/main/java/com/bernardomg/association/member/usecase/service/DefaultMemberService.java
+++ src/main/java/com/bernardomg/association/member/usecase/service/DefaultMemberService.java
@@ -1,16 +1,10 @@
 
 package com.bernardomg.association.member.usecase.service;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
-import org.springframework.data.domain.Sort.Order;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -42,16 +36,12 @@ public final class DefaultMemberService implements MemberService {
 
     @Override
     public final Iterable<Member> getAll(final MemberQuery query, final Pageable pageable) {
-        final Pageable pagination;
-
         log.debug("Reading public members with pagination {}", pageable);
 
-        pagination = correctPagination(pageable);
-
         return switch (query.getStatus()) {
-            case ACTIVE -> memberRepository.findActive(pagination);
-            case INACTIVE -> memberRepository.findInactive(pagination);
-            default -> memberRepository.findAll(pagination);
+            case ACTIVE -> memberRepository.findActive(pageable);
+            case INACTIVE -> memberRepository.findInactive(pageable);
+            default -> memberRepository.findAll(pageable);
         };
     }
 
@@ -68,53 +58,6 @@ public final class DefaultMemberService implements MemberService {
         }
 
         return member;
-    }
-
-    private final Pageable correctPagination(final Pageable pageable) {
-        final Sort     sort;
-        final Pageable page;
-
-        // TODO: change the pagination system
-        sort = correctSort(pageable.getSort());
-
-        if (pageable.isPaged()) {
-            page = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), sort);
-        } else {
-            page = Pageable.unpaged(sort);
-        }
-
-        return page;
-    }
-
-    private final Sort correctSort(final Sort received) {
-        final Optional<Order> fullNameOrder;
-        final List<Order>     orders;
-        final List<Order>     validOrders;
-
-        // Full name
-        // TODO: just order from the frontend by first and last names
-        fullNameOrder = received.stream()
-            .filter(o -> "fullName".equals(o.getProperty()))
-            .findFirst();
-
-        orders = new ArrayList<>();
-        if (fullNameOrder.isPresent()) {
-            if (Direction.ASC.equals(fullNameOrder.get()
-                .getDirection())) {
-                orders.add(Order.asc("firstName"));
-                orders.add(Order.asc("lastName"));
-            } else {
-                orders.add(Order.desc("firstName"));
-                orders.add(Order.desc("lastName"));
-            }
-        }
-
-        validOrders = received.stream()
-            .filter(o -> !"fullName".equals(o.getProperty()))
-            .toList();
-        orders.addAll(validOrders);
-
-        return Sort.by(orders);
     }
 
 }
