c10f0b98b1699f24aa6796eb34402034f561cbcb
Bernardo-MG <programming@bernardomg.com>
2024-11-11 00:31:58 +0100

    Clean person caches on fee update

--- src/main/java/com/bernardomg/association/fee/adapter/outbound/rest/controller/FeeController.java
+++ src/main/java/com/bernardomg/association/fee/adapter/outbound/rest/controller/FeeController.java
@@ -49,6 +49,7 @@ import com.bernardomg.association.fee.domain.model.Fee;
 import com.bernardomg.association.fee.domain.model.FeeQuery;
 import com.bernardomg.association.fee.usecase.service.FeeService;
 import com.bernardomg.association.member.adapter.outbound.cache.MembersCaches;
+import com.bernardomg.association.person.adapter.outbound.cache.PersonsCaches;
 import com.bernardomg.association.transaction.adapter.outbound.cache.TransactionCaches;
 import com.bernardomg.security.access.RequireResourceAccess;
 import com.bernardomg.security.permission.data.constant.Actions;
@@ -77,13 +78,14 @@ public class FeeController {
     @RequireResourceAccess(resource = "FEE", action = Actions.CREATE)
     @Caching(evict = { @CacheEvict(cacheNames = {
             // Fee caches
-            FeeCaches.FEES,
+            FeeCaches.FEES, FeeCaches.CALENDAR, FeeCaches.CALENDAR_RANGE,
             // Funds caches
             TransactionCaches.TRANSACTIONS, TransactionCaches.TRANSACTION, TransactionCaches.BALANCE,
             TransactionCaches.MONTHLY_BALANCE, TransactionCaches.CALENDAR, TransactionCaches.CALENDAR_RANGE,
             // Member caches
-            MembersCaches.MONTHLY_BALANCE, MembersCaches.MEMBERS, MembersCaches.MEMBER, FeeCaches.CALENDAR,
-            FeeCaches.CALENDAR_RANGE }, allEntries = true) })
+            MembersCaches.MONTHLY_BALANCE, MembersCaches.MEMBERS, MembersCaches.MEMBER,
+            // Person caches
+            PersonsCaches.PERSON, PersonsCaches.PERSONS }, allEntries = true) })
     public Fee create(@Valid @RequestBody final FeeCreation fee) {
         return service.createUnpaidFee(fee.getMonth(), fee.getMember()
             .getNumber());
@@ -94,12 +96,13 @@ public class FeeController {
     @Caching(evict = { @CacheEvict(cacheNames = { FeeCaches.FEE }, key = "#p0.toString() + ':' + #p1"),
             @CacheEvict(cacheNames = {
                     // Fee caches
-                    FeeCaches.FEES,
+                    FeeCaches.FEES, FeeCaches.CALENDAR, FeeCaches.CALENDAR_RANGE,
                     // Funds caches
                     MembersCaches.MONTHLY_BALANCE,
                     // Member caches
-                    MembersCaches.MEMBERS, MembersCaches.MEMBER, FeeCaches.CALENDAR, FeeCaches.CALENDAR_RANGE },
-                    allEntries = true) })
+                    MembersCaches.MEMBERS, MembersCaches.MEMBER,
+                    // Person caches
+                    PersonsCaches.PERSON, PersonsCaches.PERSONS }, allEntries = true) })
     public void delete(@PathVariable("date") final YearMonth date,
             @PathVariable("memberNumber") final long memberNumber) {
         service.delete(memberNumber, date);
@@ -110,13 +113,14 @@ public class FeeController {
     @RequireResourceAccess(resource = "FEE", action = Actions.CREATE)
     @Caching(evict = { @CacheEvict(cacheNames = {
             // Fee caches
-            FeeCaches.FEES,
+            FeeCaches.FEES, FeeCaches.CALENDAR, FeeCaches.CALENDAR_RANGE,
             // Funds caches
             TransactionCaches.TRANSACTIONS, TransactionCaches.TRANSACTION, TransactionCaches.BALANCE,
             TransactionCaches.MONTHLY_BALANCE, TransactionCaches.CALENDAR, TransactionCaches.CALENDAR_RANGE,
             // Member caches
-            MembersCaches.MONTHLY_BALANCE, MembersCaches.MEMBERS, MembersCaches.MEMBER, FeeCaches.CALENDAR,
-            FeeCaches.CALENDAR_RANGE }, allEntries = true) })
+            MembersCaches.MONTHLY_BALANCE, MembersCaches.MEMBERS, MembersCaches.MEMBER,
+            // Person caches
+            PersonsCaches.PERSON, PersonsCaches.PERSONS }, allEntries = true) })
     public Collection<Fee> pay(@Valid @RequestBody final FeePayment payment) {
         return service.payFees(payment.getFeeDates(), payment.getMember()
             .getNumber(),
