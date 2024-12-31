903d9edcc59ab1c06369edbfe3430aa9d79d122e
Bernardo-MG <programming@bernardomg.com>
2025-01-01 00:20:53 +0100

    Updated test scripts

--- src/test/resources/db/queries/library/book/donation_no_donors.sql
+++ src/test/resources/db/queries/library/book/donation_no_donors.sql
@@ -1,6 +1,3 @@
-INSERT INTO inventory.books (id, number, supertitle, title, subtitle, isbn, publish_date, donation_date, language) VALUES
-   (1, 1, 'Supertitle', 'Title', 'Subtitle', '1-56619-909-3', '1990-01-01', '1995-01-02', 'en');
-
 INSERT INTO inventory.authors (id, number, name) VALUES
    (1, 1, 'Author');
 
@@ -13,14 +10,11 @@ INSERT INTO inventory.book_types (id, number, name) VALUES
 INSERT INTO inventory.game_systems (id, number, name) VALUES
    (1, 1, 'Game system');
 
+INSERT INTO inventory.books (id, number, supertitle, title, subtitle, isbn, publish_date, donation_date, language, game_system_id, book_type_id) VALUES
+   (1, 1, 'Supertitle', 'Title', 'Subtitle', '1-56619-909-3', '1990-01-01', '1995-01-02', 'en', 1, 1);
+
 INSERT INTO inventory.book_authors (book_id, author_id) VALUES
    (1, 1);
 
 INSERT INTO inventory.book_publishers (book_id, publisher_id) VALUES
-   (1, 1);
-
-INSERT INTO inventory.book_book_types (book_id, book_type_id) VALUES
-   (1, 1);
-
-INSERT INTO inventory.book_game_systems (book_id, game_system_id) VALUES
    (1, 1);
