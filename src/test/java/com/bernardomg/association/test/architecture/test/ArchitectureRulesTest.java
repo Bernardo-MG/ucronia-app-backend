35753553fa4508ef3d040f62b44edfa2819eec29
Bernardo-MG <programming@bernardomg.com>
2025-02-13 22:56:49 +0100

    Updated architecture tests

--- src/test/java/com/bernardomg/association/test/architecture/test/ArchitectureRulesTest.java
+++ src/test/java/com/bernardomg/association/test/architecture/test/ArchitectureRulesTest.java
@@ -4,17 +4,18 @@ package com.bernardomg.association.test.architecture.test;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
 import com.bernardomg.association.test.architecture.rule.SecurityRules;
-import com.bernardomg.framework.testing.architecture.predicates.Predicates;
+import com.bernardomg.framework.testing.architecture.predicates.IsInServicePackage;
+import com.bernardomg.framework.testing.architecture.predicates.springframework.IsRepositoryNotSpringClass;
 import com.bernardomg.framework.testing.architecture.rule.CacheRules;
 import com.bernardomg.framework.testing.architecture.rule.CodingRules;
-import com.bernardomg.framework.testing.architecture.rule.ConfigurationRules;
-import com.bernardomg.framework.testing.architecture.rule.ControllerRules;
 import com.bernardomg.framework.testing.architecture.rule.DependencyRules;
 import com.bernardomg.framework.testing.architecture.rule.JpaEntityRules;
 import com.bernardomg.framework.testing.architecture.rule.RepositoryRules;
 import com.bernardomg.framework.testing.architecture.rule.ServiceRules;
 import com.bernardomg.framework.testing.architecture.rule.TransactionalRules;
 import com.bernardomg.framework.testing.architecture.rule.ValidationRules;
+import com.bernardomg.framework.testing.architecture.rule.springframework.SpringControllerRules;
+import com.bernardomg.framework.testing.architecture.rule.springframework.SpringRules;
 import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
@@ -36,12 +37,8 @@ public class ArchitectureRulesTest {
         .in(CodingRules.class);
 
     @ArchTest
-    static final ArchTests configurationRules                                            = ArchTests
-        .in(ConfigurationRules.class);
-
-    @ArchTest
     static final ArchTests controllerRules                                               = ArchTests
-        .in(ControllerRules.class);
+        .in(SpringControllerRules.class);
 
     @ArchTest
     static final ArchTests dependencyRules                                               = ArchTests
@@ -49,7 +46,7 @@ public class ArchitectureRulesTest {
 
     @ArchTest
     static final ArchRule  domain_repository_interfaces_should_not_depend_on_spring_data = noClasses()
-        .that(Predicates.areRepositoryClasses())
+        .that(new IsRepositoryNotSpringClass())
         .and()
         .areInterfaces()
         .should()
@@ -74,10 +71,14 @@ public class ArchitectureRulesTest {
 
     @ArchTest
     static final ArchRule  services_should_not_depend_on_spring_data                     = noClasses()
-        .that(Predicates.areServiceClasses())
+        .that(new IsInServicePackage())
         .should()
         .dependOnClassesThat()
         .resideInAnyPackage("org.springframework.data.domain..");
+
+    @ArchTest
+    static final ArchTests springRules                                                   = ArchTests
+        .in(SpringRules.class);
 
     @ArchTest
     static final ArchTests transactionalRules                                            = ArchTests
