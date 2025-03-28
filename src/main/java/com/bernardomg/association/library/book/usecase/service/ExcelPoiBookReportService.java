2a5bf84a7a38abee8e165da9e31b12206079ea68
Bernardo <programming@bernardomg.com>
2025-03-28 10:36:14 +0100

    Extended report to cover all books

--- src/main/java/com/bernardomg/association/library/book/usecase/service/ExcelPoiBookReportService.java
+++ src/main/java/com/bernardomg/association/library/book/usecase/service/ExcelPoiBookReportService.java
@@ -19,7 +19,9 @@ import org.springframework.transaction.annotation.Transactional;
 import com.bernardomg.association.library.author.domain.model.Author;
 import com.bernardomg.association.library.book.domain.model.Donation;
 import com.bernardomg.association.library.book.domain.model.Donor;
+import com.bernardomg.association.library.book.domain.model.FictionBook;
 import com.bernardomg.association.library.book.domain.model.GameBook;
+import com.bernardomg.association.library.book.domain.repository.FictionBookRepository;
 import com.bernardomg.association.library.book.domain.repository.GameBookRepository;
 import com.bernardomg.association.library.booktype.domain.model.BookType;
 import com.bernardomg.association.library.gamesystem.domain.model.GameSystem;
@@ -36,19 +38,24 @@ import lombok.extern.slf4j.Slf4j;
 @Transactional
 public final class ExcelPoiBookReportService implements BookReportService {
 
-    private final GameBookRepository bookRepository;
+    private final FictionBookRepository fictionBookRepository;
 
-    public ExcelPoiBookReportService(final GameBookRepository bookRepo) {
+    private final GameBookRepository    gameBookRepository;
+
+    public ExcelPoiBookReportService(final GameBookRepository gameBookRepo,
+            final FictionBookRepository fictionBookRepo) {
         super();
 
-        bookRepository = Objects.requireNonNull(bookRepo);
+        gameBookRepository = Objects.requireNonNull(gameBookRepo);
+        fictionBookRepository = Objects.requireNonNull(fictionBookRepo);
     }
 
     @Override
     public final ByteArrayOutputStream getReport() {
-        final Iterable<GameBook> books;
-        final Workbook           workbook;
-        final Sorting            sort;
+        final Iterable<GameBook>    gameBooks;
+        final Iterable<FictionBook> fictionBooks;
+        final Workbook              workbook;
+        final Sorting               sort;
 
         log.debug("Creating excel");
 
@@ -57,23 +64,109 @@ public final class ExcelPoiBookReportService implements BookReportService {
         workbook = generateWorkbook();
 
         sort = Sorting.asc("title", "language", "isbn");
-        books = bookRepository.findAll(sort);
-        loadWorkbook(workbook, books);
+        gameBooks = gameBookRepository.findAll(sort);
+        fictionBooks = fictionBookRepository.findAll(sort);
+        loadWorkbook(workbook, gameBooks, fictionBooks);
 
         return ExcelParsing.toStream(workbook);
     }
 
-    private final Workbook generateWorkbook() {
-        final XSSFWorkbook workbook;
-        final Sheet        sheet;
-        final Row          header;
-        final CellStyle    headerStyle;
-        final XSSFFont     font;
-        Cell               headerCell;
+    private final Workbook generateFictionSheet(final XSSFWorkbook workbook) {
+        final Sheet     sheet;
+        final Row       header;
+        final CellStyle headerStyle;
+        final XSSFFont  font;
+        Cell            headerCell;
 
-        workbook = new XSSFWorkbook();
+        sheet = workbook.createSheet("Ficción");
+        sheet.setColumnWidth(0, 3000);
+        sheet.setColumnWidth(1, 17000);
+        sheet.setColumnWidth(2, 3000);
+        sheet.setColumnWidth(3, 5000);
+        sheet.setColumnWidth(4, 3000);
+        sheet.setColumnWidth(4, 5000);
+        sheet.setColumnWidth(6, 5000);
+        sheet.setColumnWidth(7, 5000);
+        sheet.setColumnWidth(8, 15000);
+        sheet.setColumnWidth(9, 3000);
+        sheet.setColumnWidth(10, 5000);
+        sheet.setColumnWidth(11, 5000);
+        sheet.setColumnWidth(12, 3000);
+        sheet.setColumnWidth(13, 3000);
 
-        sheet = workbook.createSheet("Libros");
+        header = sheet.createRow(0);
+
+        headerStyle = workbook.createCellStyle();
+
+        font = workbook.createFont();
+        font.setFontName("Arial");
+        font.setFontHeightInPoints((short) 16);
+        font.setBold(true);
+        headerStyle.setFont(font);
+
+        headerCell = header.createCell(0);
+        headerCell.setCellValue("Número");
+        headerCell.setCellStyle(headerStyle);
+
+        headerCell = header.createCell(1);
+        headerCell.setCellValue("Título completo");
+        headerCell.setCellStyle(headerStyle);
+
+        headerCell = header.createCell(2);
+        headerCell.setCellValue("Idioma");
+        headerCell.setCellStyle(headerStyle);
+
+        headerCell = header.createCell(3);
+        headerCell.setCellValue("ISBN");
+        headerCell.setCellStyle(headerStyle);
+
+        headerCell = header.createCell(4);
+        headerCell.setCellValue("Publicación");
+        headerCell.setCellStyle(headerStyle);
+
+        headerCell = header.createCell(5);
+        headerCell.setCellValue("Autores");
+        headerCell.setCellStyle(headerStyle);
+
+        headerCell = header.createCell(6);
+        headerCell.setCellValue("Editores");
+        headerCell.setCellStyle(headerStyle);
+
+        headerCell = header.createCell(7);
+        headerCell.setCellValue("Donantes");
+        headerCell.setCellStyle(headerStyle);
+
+        headerCell = header.createCell(8);
+        headerCell.setCellValue("Fecha");
+        headerCell.setCellStyle(headerStyle);
+
+        headerCell = header.createCell(9);
+        headerCell.setCellValue("Prestado");
+        headerCell.setCellStyle(headerStyle);
+
+        headerCell = header.createCell(10);
+        headerCell.setCellValue("Socio");
+        headerCell.setCellStyle(headerStyle);
+
+        headerCell = header.createCell(11);
+        headerCell.setCellValue("Fecha");
+        headerCell.setCellStyle(headerStyle);
+
+        headerCell = header.createCell(12);
+        headerCell.setCellValue("Días");
+        headerCell.setCellStyle(headerStyle);
+
+        return workbook;
+    }
+
+    private final Workbook generateGamesSheet(final XSSFWorkbook workbook) {
+        final Sheet     sheet;
+        final Row       header;
+        final CellStyle headerStyle;
+        final XSSFFont  font;
+        Cell            headerCell;
+
+        sheet = workbook.createSheet("Juegos");
         sheet.setColumnWidth(0, 3000);
         sheet.setColumnWidth(1, 17000);
         sheet.setColumnWidth(2, 3000);
@@ -164,27 +257,120 @@ public final class ExcelPoiBookReportService implements BookReportService {
         return workbook;
     }
 
-    private final void loadWorkbook(final Workbook workbook, final Iterable<GameBook> books) {
-        final CellStyle  style;
-        final CellStyle  dateStyle;
-        final Sheet      sheet;
-        final DataFormat df;
-        int              index;
-        Row              row;
-        Cell             cell;
-        BookLending      lending;
-        Donation         donation;
+    private final Workbook generateWorkbook() {
+        final XSSFWorkbook workbook;
 
-        df = workbook.createDataFormat();
+        workbook = new XSSFWorkbook();
 
-        style = workbook.createCellStyle();
-        style.setWrapText(true);
+        generateGamesSheet(workbook);
+        generateFictionSheet(workbook);
 
-        dateStyle = workbook.createCellStyle();
-        dateStyle.setWrapText(true);
-        dateStyle.setDataFormat(df.getFormat("dd/MM/yyyy"));
+        return workbook;
+    }
 
-        sheet = workbook.getSheetAt(0);
+    private final void loadFiction(final Sheet sheet, final CellStyle style, final CellStyle dateStyle,
+            final Iterable<FictionBook> books) {
+        int         index;
+        Row         row;
+        Cell        cell;
+        BookLending lending;
+        Donation    donation;
+
+        index = 1;
+        for (final FictionBook book : books) {
+            row = sheet.createRow(index);
+
+            cell = row.createCell(0);
+            cell.setCellValue(book.number());
+            cell.setCellStyle(style);
+
+            cell = row.createCell(1);
+            cell.setCellValue(book.title()
+                .fullTitle());
+            cell.setCellStyle(style);
+
+            cell = row.createCell(2);
+            cell.setCellValue(book.language());
+            cell.setCellStyle(style);
+
+            cell = row.createCell(3);
+            cell.setCellValue(book.isbn());
+            cell.setCellStyle(style);
+
+            cell = row.createCell(4);
+            cell.setCellValue(book.publishDate());
+            cell.setCellStyle(dateStyle);
+
+            cell = row.createCell(6);
+            cell.setCellValue(book.authors()
+                .stream()
+                .map(Author::name)
+                .collect(Collectors.joining(", ")));
+            cell.setCellStyle(style);
+
+            cell = row.createCell(7);
+            cell.setCellValue(book.publishers()
+                .stream()
+                .map(Publisher::name)
+                .collect(Collectors.joining(", ")));
+            cell.setCellStyle(style);
+
+            if (book.donation()
+                .isPresent()) {
+                donation = book.donation()
+                    .get();
+
+                cell = row.createCell(8);
+                cell.setCellValue(donation.donors()
+                    .stream()
+                    .map(Donor::name)
+                    .map(PersonName::fullName)
+                    .collect(Collectors.joining(", ")));
+                cell.setCellStyle(style);
+
+                cell = row.createCell(9);
+                cell.setCellValue(donation.date());
+                cell.setCellStyle(dateStyle);
+            }
+
+            cell = row.createCell(10);
+            cell.setCellValue(book.lent());
+            cell.setCellStyle(style);
+
+            if (book.lent()) {
+                lending = book.lendings()
+                    .stream()
+                    .reduce((first, second) -> second)
+                    .get();
+
+                cell = row.createCell(11);
+                cell.setCellValue(lending.borrower()
+                    .name()
+                    .fullName());
+                cell.setCellStyle(style);
+
+                cell = row.createCell(12);
+                cell.setCellValue(lending.lendingDate());
+                cell.setCellStyle(dateStyle);
+
+                cell = row.createCell(13);
+                cell.setCellValue(lending.getDays());
+                cell.setCellStyle(style);
+            }
+
+            index++;
+        }
+
+    }
+
+    private final void loadGames(final Sheet sheet, final CellStyle style, final CellStyle dateStyle,
+            final Iterable<GameBook> books) {
+        int         index;
+        Row         row;
+        Cell        cell;
+        BookLending lending;
+        Donation    donation;
+
         index = 1;
         for (final GameBook book : books) {
             row = sheet.createRow(index);
@@ -281,6 +467,31 @@ public final class ExcelPoiBookReportService implements BookReportService {
 
             index++;
         }
+
+    }
+
+    private final void loadWorkbook(final Workbook workbook, final Iterable<GameBook> gameBooks,
+            final Iterable<FictionBook> fictionBooks) {
+        final CellStyle  style;
+        final CellStyle  dateStyle;
+        final Sheet      gameSheet;
+        final Sheet      fictionSheet;
+        final DataFormat df;
+
+        df = workbook.createDataFormat();
+
+        style = workbook.createCellStyle();
+        style.setWrapText(true);
+
+        dateStyle = workbook.createCellStyle();
+        dateStyle.setWrapText(true);
+        dateStyle.setDataFormat(df.getFormat("dd/MM/yyyy"));
+
+        gameSheet = workbook.getSheetAt(0);
+        loadGames(gameSheet, style, dateStyle, gameBooks);
+
+        fictionSheet = workbook.getSheetAt(1);
+        loadFiction(fictionSheet, style, dateStyle, fictionBooks);
 
     }
 
