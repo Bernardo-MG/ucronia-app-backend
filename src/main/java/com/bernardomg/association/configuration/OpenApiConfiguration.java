4e3a96429906dd63d10b4a2cc6db8a4d19dde92d
Bernardo-MG <programming@bernardomg.com>
2025-08-20 00:57:26 +0200

    Open Swagger on dev

---
+++ src/main/java/com/bernardomg/association/configuration/OpenApiConfiguration.java
@@ -0,0 +1,28 @@
+
+package com.bernardomg.association.configuration;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+
+import com.bernardomg.security.web.whitelist.WhitelistRoute;
+
+@Configuration
+@ConditionalOnProperty(
+    name = "openapi.enabled",
+    havingValue = "true"
+)
+public class OpenApiConfiguration {
+
+    @Bean("openApiWhitelist")
+    public WhitelistRoute getOpenApiWhitelist() {
+        return WhitelistRoute.of("/api-docs/**", HttpMethod.GET);
+    }
+
+    @Bean("swaggerApiWhitelist")
+    public WhitelistRoute getSwaggerApiWhitelist() {
+        return WhitelistRoute.of("/swagger-ui/**", HttpMethod.GET);
+    }
+
+}
--- src/main/java/com/bernardomg/association/configuration/WebConfiguration.java
+++ src/main/java/com/bernardomg/association/configuration/WebConfiguration.java
@@ -3,13 +3,10 @@ package com.bernardomg.association.configuration;
 
 import java.util.List;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.bernardomg.security.web.whitelist.WhitelistRoute;
 import com.bernardomg.ws.springframework.request.PaginationArgumentResolver;
 import com.bernardomg.ws.springframework.request.SortingArgumentResolver;
 
@@ -21,14 +18,5 @@ public class WebConfiguration implements WebMvcConfigurer {
         resolvers.add(new PaginationArgumentResolver());
         resolvers.add(new SortingArgumentResolver());
     }
-    @Bean("swaggerApiWhitelist")
-    public WhitelistRoute getSwaggerApiWhitelist() {
-        return WhitelistRoute.of("/swagger-ui/**", HttpMethod.GET);
-    }
-    @Bean("openApiWhitelist")
-    public WhitelistRoute getOpenApiWhitelist() {
-        return WhitelistRoute.of("/api-docs/**", HttpMethod.GET);
-    }
-
 
 }
