4b5d2761a5de0f91243fd85a18eee41a7512bd72
Bernardo-MG <programming@bernardomg.com>
2025-10-09 00:42:38 +0200

    Disable membership correctly

--- src/main/java/com/bernardomg/association/person/usecase/service/DefaultMemberStatusService.java
+++ src/main/java/com/bernardomg/association/person/usecase/service/DefaultMemberStatusService.java
@@ -4,13 +4,16 @@ package com.bernardomg.association.person.usecase.service;
 import java.time.YearMonth;
 import java.util.Collection;
 import java.util.Objects;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.bernardomg.association.person.domain.exception.MissingPersonException;
 import com.bernardomg.association.person.domain.model.Person;
+import com.bernardomg.association.person.domain.model.Person.Membership;
 import com.bernardomg.association.person.domain.repository.PersonRepository;
 
 @Service
@@ -57,6 +60,7 @@ public final class DefaultMemberStatusService implements MemberStatusService {
                 .active())
             .map(Person::number)
             .toList();
+        // TODO: activate outside the repo
         personRepository.activateAll(toActivate);
 
         toDeactivate = persons.stream()
@@ -65,16 +69,36 @@ public final class DefaultMemberStatusService implements MemberStatusService {
                 .active())
             .map(Person::number)
             .toList();
+        // TODO: deactivate outside the repo
         personRepository.deactivateAll(toDeactivate);
     }
 
     @Override
     public final void deactivate(final YearMonth date, final Long personNumber) {
+        final Optional<Person> person;
+        final Person           disabled;
+        final Membership       membership;
+
+        // If deleting at the current month, the user is set to inactive
         if (YearMonth.now()
             .equals(date)) {
-            log.debug("Deactivating member status for person {}", personNumber);
-            // If deleting at the current month, the user is set to inactive
-            personRepository.deactivate(personNumber);
+            log.debug("Deactivating membership for {}", personNumber);
+            person = personRepository.findOne(personNumber);
+
+            if (person.isEmpty()) {
+                log.warn("Missing person {}", personNumber);
+            } else {
+                membership = new Membership(false, false);
+                disabled = new Person(person.get()
+                    .identifier(), personNumber,
+                    person.get()
+                        .name(),
+                    person.get()
+                        .birthDate(),
+                    Optional.of(membership), person.get()
+                        .contacts());
+                personRepository.save(disabled);
+            }
         }
     }
 
