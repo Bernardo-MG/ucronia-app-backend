5104862112d4e15c97df9361a853d9eebbd1f75f
Bernardo-MG <programming@bernardomg.com>
2025-11-04 00:52:07 +0100

    Updated SQL

--- src/test/resources/db/queries/contact/membership_active_to_renew.sql
+++ src/test/resources/db/queries/contact/membership_active_to_renew.sql
@@ -1,2 +1,2 @@
 INSERT INTO association.contacts (id, number, first_name, last_name, identifier, birth_date, member, active, renew_membership) VALUES
-   (1, 10, 'Person 1', 'Last name 1', '6789', '1990-01-01 0:0:0', true, true, true);
+   (1, 10, 'Contact 1', 'Last name 1', '6789', '1990-01-01 0:0:0', true, true, true);
