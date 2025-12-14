69609478afb3ffbc1d2c18580c050e1b2e6805eb
Bernardo <programming@bernardomg.com>
2025-12-15 00:27:28 +0100

    Added missing test classes

---
+++ src/test/java/com/bernardomg/association/contact/test/configuration/data/annotation/PhoneContactMethod.java
@@ -0,0 +1,20 @@
+
+package com.bernardomg.association.contact.test.configuration.data.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.test.context.jdbc.Sql;
+
+@Sql("/db/queries/contact/contact_method_phone.sql")
+@Target({ ElementType.TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface PhoneContactMethod {
+
+}
