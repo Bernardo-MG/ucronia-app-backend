a11dc719e6b9b7a784898c43acb2a2132ffeb4c0
Bernardo <programming@bernardomg.com>
2025-12-15 23:06:49 +0100

    Added API

---
+++ src/main/java/com/bernardomg/association/member/adapter/outbound/rest/controller/ContactMembershipController.java
@@ -0,0 +1,83 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2022-2025 Bernardo Martínez Garrido
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.bernardomg.association.member.adapter.outbound.rest.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bernardomg.association.member.adapter.outbound.rest.model.MemberDtoMapper;
+import com.bernardomg.association.member.domain.filter.MemberFilter;
+import com.bernardomg.association.member.domain.model.Member;
+import com.bernardomg.association.member.domain.model.MemberStatus;
+import com.bernardomg.association.member.usecase.service.MemberService;
+import com.bernardomg.data.domain.Page;
+import com.bernardomg.data.domain.Pagination;
+import com.bernardomg.data.domain.Sorting;
+import com.bernardomg.data.web.WebSorting;
+import com.bernardomg.security.access.annotation.RequireResourceAuthorization;
+import com.bernardomg.security.permission.domain.constant.Actions;
+import com.bernardomg.ucronia.openapi.api.ContactMembershipApi;
+import com.bernardomg.ucronia.openapi.api.MemberApi;
+import com.bernardomg.ucronia.openapi.model.ContactResponseDto;
+import com.bernardomg.ucronia.openapi.model.MemberChangeDto;
+import com.bernardomg.ucronia.openapi.model.MemberCreationDto;
+import com.bernardomg.ucronia.openapi.model.MemberPageResponseDto;
+import com.bernardomg.ucronia.openapi.model.MemberResponseDto;
+import com.bernardomg.ucronia.openapi.model.MemberStatusDto;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
+
+/**
+ * Member REST controller.
+ *
+ * @author Bernardo Mart&iacute;nez Garrido
+ *
+ */
+@RestController
+public class ContactMembershipController implements ContactMembershipApi {
+
+    /**
+     * Member service.
+     */
+    private final MemberService service;
+
+    public ContactMembershipController(final MemberService service) {
+        super();
+
+        this.service = service;
+    }
+
+    @Override
+    @RequireResourceAuthorization(resource = "MEMBER", action = Actions.CREATE)
+    public ContactResponseDto addMembership(Long number) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
