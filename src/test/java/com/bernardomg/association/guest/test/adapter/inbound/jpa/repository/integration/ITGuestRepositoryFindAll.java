03d3996fcfb6b91979ff12ba701ffe4aeccf323a
Bernardo <programming@bernardomg.com>
2025-12-24 18:56:56 +0100

    Added tests

---
+++ src/test/java/com/bernardomg/association/guest/test/adapter/inbound/jpa/repository/integration/ITGuestRepositoryFindAll.java
@@ -0,0 +1,128 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2022-2025 Bernardo Martínez Garrido
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.bernardomg.association.guest.test.adapter.inbound.jpa.repository.integration;
+
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.InstanceOfAssertFactories;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bernardomg.association.contact.test.configuration.data.annotation.ValidContact;
+import com.bernardomg.association.guest.domain.filter.GuestFilter;
+import com.bernardomg.association.guest.domain.model.Guest;
+import com.bernardomg.association.guest.domain.repository.GuestRepository;
+import com.bernardomg.association.guest.test.configuration.data.annotation.ValidGuest;
+import com.bernardomg.association.guest.test.configuration.factory.Guests;
+import com.bernardomg.data.domain.Page;
+import com.bernardomg.data.domain.Pagination;
+import com.bernardomg.data.domain.Sorting;
+import com.bernardomg.test.configuration.annotation.IntegrationTest;
+
+@IntegrationTest
+@DisplayName("GuestRepository - find all")
+class ITGuestRepositoryFindAll {
+
+    @Autowired
+    private GuestRepository repository;
+
+    public ITGuestRepositoryFindAll() {
+        super();
+    }
+
+    @Test
+    @DisplayName("With an guest, it is returned")
+    @ValidGuest
+    void testFindAll() {
+        final Page<Guest> guests;
+        final Pagination    pagination;
+        final Sorting       sorting;
+        final GuestFilter filter;
+
+        // GIVEN
+        pagination = new Pagination(1, 10);
+        sorting = new Sorting(List.of());
+        filter = new GuestFilter("");
+
+        // WHEN
+        guests = repository.findAll(filter, pagination, sorting);
+
+        // THEN
+        Assertions.assertThat(guests)
+            .extracting(Page::content)
+            .asInstanceOf(InstanceOfAssertFactories.LIST)
+            .containsExactly(Guests.valid());
+    }
+
+    @Test
+    @DisplayName("With no guest, nothing is returned")
+    void testFindAll_NoData() {
+        final Page<Guest> guests;
+        final Pagination    pagination;
+        final Sorting       sorting;
+        final GuestFilter filter;
+
+        // GIVEN
+        pagination = new Pagination(1, 10);
+        sorting = new Sorting(List.of());
+        filter = new GuestFilter("");
+
+        // WHEN
+        guests = repository.findAll(filter, pagination, sorting);
+
+        // THEN
+        Assertions.assertThat(guests)
+            .extracting(Page::content)
+            .asInstanceOf(InstanceOfAssertFactories.LIST)
+            .isEmpty();
+    }
+
+    @Test
+    @DisplayName("With a guest with no guestship, it returns nothing")
+    @ValidContact
+    void testFindAll_NoGuestship() {
+        final Page<Guest> guests;
+        final Pagination    pagination;
+        final Sorting       sorting;
+        final GuestFilter filter;
+
+        // GIVEN
+        pagination = new Pagination(1, 10);
+        sorting = new Sorting(List.of());
+        filter = new GuestFilter("");
+
+        // WHEN
+        guests = repository.findAll(filter, pagination, sorting);
+
+        // THEN
+        Assertions.assertThat(guests)
+            .extracting(Page::content)
+            .asInstanceOf(InstanceOfAssertFactories.LIST)
+            .isEmpty();
+    }
+
+}
---
+++ src/test/java/com/bernardomg/association/member/test/adapter/inbound/jpa/repository/integration/ITMemberRepositoryFindAll.java
@@ -0,0 +1,129 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2022-2025 Bernardo Martínez Garrido
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.bernardomg.association.member.test.adapter.inbound.jpa.repository.integration;
+
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.InstanceOfAssertFactories;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bernardomg.association.contact.test.configuration.data.annotation.ValidContact;
+import com.bernardomg.association.member.domain.filter.MemberFilter;
+import com.bernardomg.association.member.domain.model.Member;
+import com.bernardomg.association.member.domain.model.MemberStatus;
+import com.bernardomg.association.member.domain.repository.MemberRepository;
+import com.bernardomg.association.member.test.configuration.data.annotation.ActiveMember;
+import com.bernardomg.association.member.test.configuration.factory.Members;
+import com.bernardomg.data.domain.Page;
+import com.bernardomg.data.domain.Pagination;
+import com.bernardomg.data.domain.Sorting;
+import com.bernardomg.test.configuration.annotation.IntegrationTest;
+
+@IntegrationTest
+@DisplayName("MemberRepository - find all")
+class ITMemberRepositoryFindAll {
+
+    @Autowired
+    private MemberRepository repository;
+
+    public ITMemberRepositoryFindAll() {
+        super();
+    }
+
+    @Test
+    @DisplayName("With an member, it is returned")
+    @ActiveMember
+    void testFindAll() {
+        final Page<Member> members;
+        final Pagination   pagination;
+        final Sorting      sorting;
+        final MemberFilter filter;
+
+        // GIVEN
+        pagination = new Pagination(1, 10);
+        sorting = new Sorting(List.of());
+        filter = new MemberFilter(MemberStatus.ALL, "");
+
+        // WHEN
+        members = repository.findAll(filter, pagination, sorting);
+
+        // THEN
+        Assertions.assertThat(members)
+            .extracting(Page::content)
+            .asInstanceOf(InstanceOfAssertFactories.LIST)
+            .containsExactly(Members.active());
+    }
+
+    @Test
+    @DisplayName("With no member, nothing is returned")
+    void testFindAll_NoData() {
+        final Page<Member> members;
+        final Pagination   pagination;
+        final Sorting      sorting;
+        final MemberFilter filter;
+
+        // GIVEN
+        pagination = new Pagination(1, 10);
+        sorting = new Sorting(List.of());
+        filter = new MemberFilter(MemberStatus.ALL, "");
+
+        // WHEN
+        members = repository.findAll(filter, pagination, sorting);
+
+        // THEN
+        Assertions.assertThat(members)
+            .extracting(Page::content)
+            .asInstanceOf(InstanceOfAssertFactories.LIST)
+            .isEmpty();
+    }
+
+    @Test
+    @DisplayName("With a member with no membership, it returns nothing")
+    @ValidContact
+    void testFindAll_NoMembership() {
+        final Page<Member> members;
+        final Pagination   pagination;
+        final Sorting      sorting;
+        final MemberFilter filter;
+
+        // GIVEN
+        pagination = new Pagination(1, 10);
+        sorting = new Sorting(List.of());
+        filter = new MemberFilter(MemberStatus.ALL, "");
+
+        // WHEN
+        members = repository.findAll(filter, pagination, sorting);
+
+        // THEN
+        Assertions.assertThat(members)
+            .extracting(Page::content)
+            .asInstanceOf(InstanceOfAssertFactories.LIST)
+            .isEmpty();
+    }
+
+}
