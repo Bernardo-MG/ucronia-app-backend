f8fada860aa22d29dd1c566d1eabb5c84303805b
Bernardo <programming@bernardomg.com>
2026-02-12 00:23:46 +0100

    Removed initial settings

--- src/main/resources/db/changelog/structure/sql/initial_settings.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO configuration.settings (code, config_value, value_type) VALUES
-  ('social.teamup.id', '', 'string'),
-  ('social.googleMap.id', '', 'string'),
-  ('social.instagram', '', 'string'),
-  ('contact.email', '', 'string');
