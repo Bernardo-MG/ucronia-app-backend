208cf6a326ff0f9eaef858917efeac9c7ab9d527
Bernardo <programming@bernardomg.com>
2026-03-23 13:57:09 +0100

    Fixed tests

---
+++ core/src/test/resources/db/queries/transaction/decimal_adds_zero.sql
@@ -0,0 +1,5 @@
+INSERT INTO funds.transactions (index, description, amount, date) VALUES
+   (1, 'Transaction 1', -40.8, '2020-01-01'),
+   (2, 'Transaction 2', 13.6, '2020-01-01'),
+   (3, 'Transaction 3', 13.6, '2020-01-01'),
+   (4, 'Transaction 4', 13.6, '2020-01-01');
