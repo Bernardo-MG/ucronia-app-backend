d9a90f9f43e8d1e23efc7de8a08b179b3cd427dd
Bernardo <programming@bernardomg.com>
2026-04-01 00:31:34 +0200

    Extracted module

---
+++ association/sponsor/domain/src/main/java/com/bernardomg/association/sponsor/domain/model/SponsorConstants.java
@@ -0,0 +1,8 @@
+
+package com.bernardomg.association.sponsor.domain.model;
+
+public final class SponsorConstants {
+
+    public static final String PROFILE_TYPE = "sponsor";
+
+}
---
+++ association/sponsor/spring-jpa-adapter/src/test/java/com/bernardomg/association/sponsor/test/factory/SponsorEntities.java
@@ -0,0 +1,83 @@
+
+package com.bernardomg.association.sponsor.test.factory;
+
+import java.util.List;
+import java.util.Set;
+
+import com.bernardomg.association.profile.test.configuration.factory.ProfileEntities;
+import com.bernardomg.association.sponsor.adapter.inbound.jpa.model.SponsorEntity;
+import com.bernardomg.association.sponsor.adapter.inbound.jpa.model.SponsorEntityConstants;
+import com.bernardomg.association.sponsor.test.configuration.factory.SponsorConstants;
+
+public final class SponsorEntities {
+
+    public static final SponsorEntity created() {
+        final SponsorEntity entity;
+
+        entity = new SponsorEntity();
+        entity.setId(1L);
+        entity.setProfile(ProfileEntities.valid());
+        entity.setYears(List.of(SponsorConstants.YEAR));
+        entity.getProfile()
+            .setTypes(Set.of(SponsorEntityConstants.PROFILE_TYPE));
+
+        return entity;
+    }
+
+    public static final SponsorEntity createdWithEmail() {
+        final SponsorEntity entity;
+
+        entity = new SponsorEntity();
+        entity.setId(1L);
+        entity.setProfile(ProfileEntities.withEmail());
+        entity.setYears(List.of(SponsorConstants.YEAR));
+        entity.getProfile()
+            .setTypes(Set.of(SponsorEntityConstants.PROFILE_TYPE));
+
+        return entity;
+    }
+
+    public static final SponsorEntity firstNameChange() {
+        final SponsorEntity entity;
+
+        entity = new SponsorEntity();
+        entity.setId(1L);
+        entity.setProfile(ProfileEntities.firstNameChange());
+        entity.setYears(List.of(SponsorConstants.YEAR));
+        entity.getProfile()
+            .setTypes(Set.of(SponsorEntityConstants.PROFILE_TYPE));
+
+        return entity;
+    }
+
+    public static final SponsorEntity valid() {
+        final SponsorEntity entity;
+
+        entity = new SponsorEntity();
+        entity.setId(1L);
+        entity.setProfile(ProfileEntities.valid());
+        entity.setYears(List.of(SponsorConstants.YEAR));
+        entity.getProfile()
+            .setTypes(Set.of(SponsorEntityConstants.PROFILE_TYPE));
+
+        return entity;
+    }
+
+    public static final SponsorEntity withEmail() {
+        final SponsorEntity entity;
+
+        entity = new SponsorEntity();
+        entity.setId(1L);
+        entity.setProfile(ProfileEntities.withEmail());
+        entity.setYears(List.of(SponsorConstants.YEAR));
+        entity.getProfile()
+            .setTypes(Set.of(SponsorEntityConstants.PROFILE_TYPE));
+
+        return entity;
+    }
+
+    private SponsorEntities() {
+        super();
+    }
+
+}
