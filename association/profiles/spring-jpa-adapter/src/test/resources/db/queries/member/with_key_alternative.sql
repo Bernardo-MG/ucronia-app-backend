73e79d66f36554319637a44221b98f04d41d7ce1
Bernardo <programming@bernardomg.com>
2026-08-11 23:51:30 +0200

    Corrected initialization and test

--- association/profiles/spring-jpa-adapter/src/test/resources/db/queries/member/with_key_alternative.sql
+++ association/profiles/spring-jpa-adapter/src/test/resources/db/queries/member/with_key_alternative.sql
@@ -3,4 +3,4 @@ INSERT INTO inventory.keys (id, number, missing, description) VALUES
 INSERT INTO directory.profiles (id, number, first_name, last_name, identifier, birth_date, address, comments, types) VALUES
    (2, 20, 'Name 2', 'Last name 2', '67890', '1990-01-01 0:0:0', 'Address', 'Comments', '["member"]');
 INSERT INTO directory.members (id, fee_type_id, active, renew_membership, key_number) VALUES
-   (2, 1, true, true, 100);
+   (2, 1, true, true, 1);
