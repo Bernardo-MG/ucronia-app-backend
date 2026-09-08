ee6fdaba7aeeb71693bcb4e0aaa764393a45087a
Bernardo <programming@bernardomg.com>
2026-09-08 23:47:50 +0200

    Fixed endpoint

--- docker/localstack/init-s3.py
+++ docker/localstack/init-s3.py
@@ -7,9 +7,11 @@ from botocore.exceptions import ClientError
 
 bucket = os.getenv("IMAGE_S3_BUCKET", "ucronia-images")
 region = os.getenv("AWS_DEFAULT_REGION", "eu-west-1")
+endpoint = os.getenv("AWS_ENDPOINT_URL", "http://s3:4566")
+
 s3 = boto3.client(
     "s3",
-    endpoint_url="http://localhost:4566",
+    endpoint_url=endpoint,
     region_name=region,
     aws_access_key_id="test",
     aws_secret_access_key="test",
