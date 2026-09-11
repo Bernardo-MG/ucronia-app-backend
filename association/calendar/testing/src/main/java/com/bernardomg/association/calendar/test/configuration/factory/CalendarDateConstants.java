69e80716487e549bc6d254e3d7c1bc286b780f8c
Bernardo <programming@bernardomg.com>
2026-09-11 12:25:07 +0200

    Corrected test data

--- association/calendar/testing/src/main/java/com/bernardomg/association/calendar/test/configuration/factory/CalendarDateConstants.java
+++ association/calendar/testing/src/main/java/com/bernardomg/association/calendar/test/configuration/factory/CalendarDateConstants.java
@@ -16,7 +16,7 @@ public final class CalendarDateConstants {
         .plus(2L, ChronoUnit.DAYS)
         .plus(5L, ChronoUnit.HOURS);
 
-    public static final Instant FROM         = LocalDateTime.of(2020, Month.JANUARY, 1, 12, 0)
+    public static final Instant FROM         = LocalDateTime.of(2020, Month.JANUARY, 12, 21, 0)
         .toInstant(ZoneOffset.UTC);
 
     public static final Instant START        = LocalDateTime.of(2020, Month.FEBRUARY, 1, 14, 0)
@@ -25,7 +25,7 @@ public final class CalendarDateConstants {
     public static final Instant START_FUTURE = Instant.now()
         .plus(2L, ChronoUnit.DAYS);
 
-    public static final Instant TO           = LocalDateTime.of(2020, Month.JANUARY, 1, 14, 0)
+    public static final Instant TO           = LocalDateTime.of(2020, Month.JANUARY, 14, 21, 0)
         .toInstant(ZoneOffset.UTC);
 
     private CalendarDateConstants() {
